9bfd09813b80f26ea396ff09622d795826183cad
Nakata Nobuyuki <nobu.nakata@gmail.com>
2016-01-22 02:51:49 +0900

    焚き火

---
+++ Assets/Packages/WaterColorFilter/Examples/DebugUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace WaterColorFilterSystem {
+	public class DebugUI : MonoBehaviour {
+		ToneFilter _tone;
+		WaterColorFilter _water;
+
+		Rect _window = new Rect(10, 10, 80, 50);
+
+		void OnEnable() {
+			_tone = GetComponent<ToneFilter> ();
+			_water = GetComponent<WaterColorFilter> ();
+		}
+		void OnGUI() {
+			_window = GUILayout.Window (0, _window, Window, "Debug UI");
+		}
+		void Window(int id) {
+			_tone.enabled = GUILayout.Toggle (_tone.enabled, "Tone-Based Shading");
+			_water.enabled = GUILayout.Toggle (_water.enabled, "WaterColor Filter");
+
+			GUI.DragWindow ();
+		}
+	}
+}
